b7ba32c5e16a469f9de259211d6768d614f0883a
Gokulakrishna05 <kesavamoorthi007>
2024-12-06 11:10:15 +0530

    Complete

--- RetailSales/Controllers/Master/CountryController.cs
+++ RetailSales/Controllers/Master/CountryController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RetailSales.Interface.Master;
+using RetailSales.Models;
+using System.Data;
 
 namespace RetailSales.Controllers.Master
 {
@@ -10,24 +12,104 @@ namespace RetailSales.Controllers.Master
         {
             CountryService = _CountryService;
         }
-        //public IActionResult Country(string id)
-        //{
-        //    Country ic = new Country();
-        //    if (id == null)
-        //    {
+        public IActionResult Country(string id)
+        {
+            Country ic = new Country();
+
+            if (id == null)
+            {
+
+            }
+            else
+            {
+                //ic = CountryService.GetCountryById(id);
+
+            }
+            return View(ic);
+        }
+        [HttpPost]
+        public ActionResult Country(Country Ic, string id)
+        {
+
+            try
+            {
+                Ic.ID = id;
+                string Strout = CountryService.CountryCRUD(Ic);
+                if (string.IsNullOrEmpty(Strout))
+                {
+                    if (Ic.ID == null)
+                    {
+                        TempData["notice"] = "Country Inserted Successfully...!";
+                    }
+                    else
+                    {
+                        TempData["notice"] = "Country Updated Successfully...!";
+                    }
+                    return RedirectToAction("Country");
+                }
+
+                else
+                {
+                    ViewBag.PageTitle = "Edit Country";
+                    TempData["notice"] = Strout;
+                    //return View();
+                }
+
+                // }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return View(Ic);
+        }
+        public IActionResult ListCountry()
+        {
+            return View();
+        }
+        public ActionResult MyListCountrygrid(string strStatus)
+        {
+            List<Countrygrid> Reg = new List<Countrygrid>();
+            DataTable dtUsers = new DataTable();
+            strStatus = strStatus == "" ? "Y" : strStatus;
+            dtUsers = CountryService.GetAllCountryGRID(strStatus);
+            for (int i = 0; i < dtUsers.Rows.Count; i++)
+            {
+
+                string DeleteRow = string.Empty;
+                string EditRow = string.Empty;
+
+                if (dtUsers.Rows[i]["IS_ACTIVE"].ToString() == "Y")
+                {
+
+                    EditRow = "<a href=Country?id=" + dtUsers.Rows[i]["ID"].ToString() + "><img src='../Images/edit.png' alt='Edit' /></a>";
+                    DeleteRow = "DeleteMR?tag=Del&id=" + dtUsers.Rows[i]["ID"].ToString() + "";
+                }
+                else
+                {
+                    EditRow = "";
+                    DeleteRow = "Remove?tag=Del&id=" + dtUsers.Rows[i]["ID"].ToString() + "";
+                }
 
 
-        //    }
-        //    else
-        //    {
-                
-        //        //ic = CountryService.GetCountryById(id);
+                Reg.Add(new Countrygrid
+                {
+                    id = dtUsers.Rows[i]["ID"].ToString(),
+                    coname = dtUsers.Rows[i]["COUNTRY_NAME"].ToString(),
+                    concode = dtUsers.Rows[i]["COUNTRY_CODE"].ToString(),
+                    editrow = EditRow,
+                    delrow = DeleteRow,
 
-        //    }
+                });
+            }
 
+            return Json(new
+            {
+                Reg
+            });
 
-        //    return View(ic);
-        //}
-        
+        }
+
     }
 }
--- RetailSales/Interface/Master/ICountryService.cs
+++ RetailSales/Interface/Master/ICountryService.cs
@@ -1,6 +1,11 @@
-﻿namespace RetailSales.Interface.Master
+﻿using RetailSales.Models;
+using System.Data;
+
+namespace RetailSales.Interface.Master
 {
     public interface ICountryService
     {
+        string CountryCRUD(Country ic);
+        DataTable GetAllCountryGRID(string strStatus);
     }
 }
