cfd44ca62cfbb38e36b4057892f84a1cd2778be1
kesavamoorthi007 <kesavamoorthi70@gmail.com>
2025-01-24 13:38:57 +0530

    Complete

--- RetailSales/Controllers/Master/ProductdetailController.cs
+++ RetailSales/Controllers/Master/ProductdetailController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using RetailSales.Interface.Master;
 using RetailSales.Models;
+using RetailSales.Services.Master;
 using System.Data;
 
 namespace RetailSales.Controllers.Master
@@ -19,6 +20,7 @@ namespace RetailSales.Controllers.Master
             Productdetail ic = new Productdetail();
 
             ic.Categorylst = BindCategory();
+            ic.Uomlst = BindUOM();
 
             if (id == null)
             {
@@ -47,6 +49,59 @@ namespace RetailSales.Controllers.Master
             {
                 throw ex;
             }
+        }
+        public List<SelectListItem> BindUOM()
+        {
+            try
+            {
+                DataTable dtDesg = ProductdetailService.GetUom();
+                List<SelectListItem> lstdesg = new List<SelectListItem>();
+                for (int i = 0; i < dtDesg.Rows.Count; i++)
+                {
+                    lstdesg.Add(new SelectListItem() { Text = dtDesg.Rows[i]["UOM_CODE"].ToString(), Value = dtDesg.Rows[i]["ID"].ToString() });
+                }
+                return lstdesg;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public IActionResult ListProductdetail()
+        {
+            return View();
+        }
+        public ActionResult MyListProductdetailgrid()
+        {
+            List<Productdetailgrid> Reg = new List<Productdetailgrid>();
+            DataTable dtUsers = new DataTable();
+            dtUsers = ProductdetailService.GetAllProductDeatilsGRID();
+            for (int i = 0; i < dtUsers.Rows.Count; i++)
+            {
+
+                string DeleteRow = string.Empty;
+                string EditRow = string.Empty;
+                EditRow = "<a><img src='../Images/edit.png' alt='Edit'  /></a>";
+                DeleteRow = "<a><img src='../Images/Inactive.png' alt='Deactivate'  /></a>";
+
+                Reg.Add(new Productdetailgrid
+                {
+                    id = dtUsers.Rows[i]["ID"].ToString(),
+                    product = dtUsers.Rows[i]["PRODUCT_NAME"].ToString(),
+                    varint = dtUsers.Rows[i]["VARIANT"].ToString(),
+                    uom = dtUsers.Rows[i]["UOM"].ToString(),
+                    rate = dtUsers.Rows[i]["RATE"].ToString(),
+                    editrow = EditRow,
+                    delrow = DeleteRow,
+
+                });
+            }
+
+            return Json(new
+            {
+                Reg
+            });
+
         }
 
     }
--- RetailSales/Models/Master/Productdetail.cs
+++ RetailSales/Models/Master/Productdetail.cs
@@ -7,15 +7,30 @@ namespace RetailSales.Models
         public Productdetail()
         {
             this.Categorylst = new List<SelectListItem>();
+            this.Uomlst = new List<SelectListItem>();
         }
         public List<SelectListItem> Categorylst;
+        public List<SelectListItem> Uomlst;
         public string Product { get; set; }
         public string Varint { get; set; }
         public string Productdescription { get; set; }
         public string Hsncode { get; set; }
         public string Productlist { get; set; }
         public string Uom { get; set; }
+        public string Rate { get; set; }
+        public string ddlStatus { get; set; }
 
+
+    }
+    public class Productdetailgrid
+    {
+        public string id { get; set; }
+        public string product { get; set; }
+        public string varint { get; set; }
+        public string uom { get; set; }
+        public string rate { get; set; }
+        public string editrow { get; set; }
+        public string delrow { get; set; }
 
     }
 }
