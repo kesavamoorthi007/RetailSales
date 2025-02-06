af3341a42da9a1d8aa5f863975cf4fa7f7509270
kesavamoorthi007 <kesavanmoorthi70@gmail.com>
2025-02-06 18:14:09 +0530

    rate view page

---
+++ RetailSales/Interface/Master/IRateService.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+namespace RetailSales.Interface.Master
+{
+    public interface IRateService
+    {
+        public DataTable GetItem();
+
+        public DataTable GetItemDetails(string ItemId);
+    }
+}
---
+++ RetailSales/Models/Master/Rate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RetailSales.Models.Master
+{
+    public class Rate
+    {
+        public string ID { get; set; }
+        public string DocNo { get; set; }
+        public string DocDate { get; set; }
+        public string ValidFrom { get; set; }
+        public string ValidTo { get; set; }
+        public List<RateList> RateList { get; set; }
+        public string ddlStatus { get; set; }
+    }
+
+    public class RateList
+    {
+        public string Isvalid { get; set; }
+        public string Item { get; set; }
+        public List<SelectListItem> Itemlst { get; set; }
+        public string saveItemId { get; set; }
+        public string Unit { get; set; }
+        public string Rate1 { get; set; }
+    }
+}
