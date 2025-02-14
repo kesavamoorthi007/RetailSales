e885fc49e3a234130828028313430c1b791c019b
devteam845 <devteam@icand.in>
2025-02-14 18:18:44 +0530

    Ledger Complete

--- RetailSales/Interface/Accounts/ILedgersServices.cs
+++ RetailSales/Interface/Accounts/ILedgersServices.cs
@@ -5,21 +5,18 @@ namespace RetailSales.Interface.Accounts
 {
     public interface ILedgersServices
     {
-        DataTable GetLedgers();
+        //DataTable GetLedgers();
+        DataTable GetAccountName();
 
         DataTable GetEditLedgersDetail(string id);
 
-        DataTable GetAllLedgersGRID(string strStatus);
+        string LedgersCRUD(Ledgers cy);
 
-        DataTable GetAccountName();
+        DataTable GetAllLedgersGRID(string strStatus);       
 
         string StatusChange(string tag, string id);
 
         string RemoveChange(string tag, string id);
-
-        string LedgersCRUD(Ledgers ic);
-
-        
 
         //string LedgersCRUD(Models.Accounts.Ledgers ic);
 
