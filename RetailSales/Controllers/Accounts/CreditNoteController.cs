0b96f814318629ff3903a408cbbb070e8fdc1546
kesavamoorthi007 <kesavanmoorthi70@gmail.com>
2025-02-20 17:02:28 +0530

    credit

--- RetailSales/Controllers/Accounts/CreditNoteController.cs
+++ RetailSales/Controllers/Accounts/CreditNoteController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using RetailSales.Interface.Accounts;
+using RetailSales.Interface.Purchase;
 using RetailSales.Models;
 using RetailSales.Models.Accounts;
 using RetailSales.Services.Accounts;
+using RetailSales.Services.Inventory;
 using RetailSales.Services.Purchase;
 using System.Data;
 
@@ -11,35 +13,85 @@ namespace RetailSales.Controllers.Accounts
 {
     public class CreditNoteController : Controller
     {
+        
+        IConfiguration? _configuratio;
+        private string? _connectionString;
+        DataTransactions datatrans;
         ICreditNoteService CreditNoteService;
-        public CreditNoteController(ICreditNoteService _CreditNoteService)
+        public CreditNoteController(ICreditNoteService _CreditNoteService, IConfiguration _configuratio)
         {
+            _connectionString = _configuratio.GetConnectionString("MySqlConnection");
+            datatrans = new DataTransactions(_connectionString);
             CreditNoteService = _CreditNoteService;
         }
+
+        
+       
         public IActionResult CreditNote(string id)
         {
             CreditNote ic = new CreditNote();
             ic.ExcRate = "1";
             ic.RefDate = DateTime.Now.ToString("dd-MMM-yyyy");
             ic.VocDate = DateTime.Now.ToString("dd-MMM-yyyy");
-
+            DataTable dtv = datatrans.GetSequence("PurchaseOrder");
+            if (dtv.Rows.Count > 0)
+            {
+                ic.VocNo = dtv.Rows[0]["PREFIX"].ToString() + "/" + dtv.Rows[0]["SUFFIX"].ToString() + "/" + dtv.Rows[0]["last"].ToString();
+            }
             CreditNoteItem tda = new CreditNoteItem();
             List<CreditNoteItem> TData = new List<CreditNoteItem>();
             if (id == null)
             {
-                for (int i = 0; i < 1; i++)
+                for (int i = 0; i < 2; i++)
                 {
                     tda = new CreditNoteItem();
                     tda.Isvalid = "Y";
                     tda.DBCRlst = BindDbCr();
-                    tda.AccNamelst = BindAcc();
-                    tda.DBCR = "Dr";
+                    tda.AccNamelst = BindAcc("");                    
+;                   tda.DBCR = "Dr";
                     TData.Add(tda);
                 }
             }
             else
             {
+                DataTable dt = new DataTable();
+                dt = CreditNoteService.GetEditCreditNote(id);
+                if (dt.Rows.Count > 0)
+                {
+                    ic.VocNo = dt.Rows[0]["PONO"].ToString();
+                    ic.VocDate = dt.Rows[0]["PODATE"].ToString();
+                    //ic.Suplst = BindSupplier();
+                    ic.exchangeRate = dt.Rows[0]["SUP_NAME"].ToString();
+                    ic.RefNo = dt.Rows[0]["ADDRESS"].ToString();
+                    ic.RefDate = dt.Rows[0]["COUNTRY"].ToString();
+                    ic.Currency = dt.Rows[0]["STATE"].ToString();
+                    ic.totdeb = dt.Rows[0]["CITY"].ToString();
+                    ic.totcri = dt.Rows[0]["REF_NO"].ToString();
+                    ic.totamt = dt.Rows[0]["REF_DATE"].ToString();
+                    ic.AmtWd = dt.Rows[0]["AMTINWORDS"].ToString();
+                    ic.Narr = dt.Rows[0]["NARRATION"].ToString();                
+                    ic.ID = id;
 
+                }
+                DataTable dtt = new DataTable();
+                dtt = CreditNoteService.GetCreditNoteDetailes(id);
+                if (dtt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dtt.Rows.Count; i++)
+                    {
+                        tda = new CreditNoteItem();
+                        //tda.Itemlst = BindItem();
+                        tda.DBCR = dtt.Rows[i]["ITEM"].ToString();
+                        //tda.AccName = BindVarient(tda.Item);
+                        tda.AccName = dtt.Rows[i]["VARIANT"].ToString();                       
+                        tda.DebitAmt = Convert.ToDouble(dtt.Rows[i]["TARIFF"]);
+                        tda.CreditAmt = Convert.ToDouble(dtt.Rows[i]["TARIFF"]);
+                        tda.Balance = dtt.Rows[i]["QTY"].ToString();                      
+                        tda.ID = id;
+                        tda.Isvalid = "Y";
+                        TData.Add(tda);
+                    }
+                }
             }
             ic.CreditNotelst = TData;
             return View(ic);
@@ -58,11 +110,12 @@ namespace RetailSales.Controllers.Accounts
                 throw ex;
             }
         }
-        public List<SelectListItem> BindAcc()
+       
+        public List<SelectListItem> BindAcc(string id)
         {
             try
             {
-                DataTable dtDesg = CreditNoteService.GetAcc();
+                DataTable dtDesg = CreditNoteService.GetAcc(id);
                 List<SelectListItem> lstdesg = new List<SelectListItem>();
                 for (int i = 0; i < dtDesg.Rows.Count; i++)
                 {
@@ -77,18 +130,41 @@ namespace RetailSales.Controllers.Accounts
                 throw ex;
             }
         }
+
+        public ActionResult GetLedgerDetails(string ItemId)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                string balance = "";               
+                dt = CreditNoteService.GetLedgerDetails(ItemId);
+
+                if (dt.Rows.Count > 0)
+                {
+                    balance = dt.Rows[0]["CLOSE_BAL"].ToString();
+                }
+
+                var result = new { balance = balance};
+                return Json(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
         public JsonResult GetDebJSON()
         {
             CreditNoteItem model = new CreditNoteItem();
             return Json(BindDbCr());
 
         }
-        public JsonResult GetAccJSON()
+        public JsonResult GetAccJSON(string id)
         {
             CreditNoteItem model = new CreditNoteItem();
-            return Json(BindAcc());
-
-        }     
+            return Json(BindAcc(id));
+        }
         [HttpPost]
         public ActionResult CreditNote(CreditNote cy, string id)
         {
@@ -107,7 +183,7 @@ namespace RetailSales.Controllers.Accounts
                     {
                         TempData["notice"] = "CreditNote Updated Successfully...!";
                     }
-                    return RedirectToAction("ListPurchaseorder");
+                    return RedirectToAction("CreditNote");
                 }
 
                 else
--- RetailSales/Services/Accounts/CreditNoteService.cs
+++ RetailSales/Services/Accounts/CreditNoteService.cs
@@ -14,96 +14,160 @@ namespace RetailSales.Services.Accounts
             _connectionString = _configuratio.GetConnectionString("MySqlConnection");
             datatrans = new DataTransactions(_connectionString);
         }
-        public DataTable GetAcc()
+        public DataTable GetAcc(string id)
         {
             string SvSql = string.Empty;
-            SvSql = "Select LEDGER_NAME,ID FROM ACC_LEDGER  ";
+            SvSql = "Select LEDGER_NAME,ID FROM ACC_LEDGER WHERE ACC_GRP_CODE ='026' ";
             DataTable dtt = new DataTable();
             SqlDataAdapter adapter = new SqlDataAdapter(SvSql, _connectionString);
             SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
             adapter.Fill(dtt);
             return dtt;
         }
-
+        
+        public DataTable GetLedgerDetails(string ItemId)
+        {
+            string SvSql = string.Empty;
+            SvSql = " SELECT CLOSE_BAL FROM ACC_LEDGER  WHERE ID='" + ItemId + "'";
+            DataTable dtt = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(SvSql, _connectionString);
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            adapter.Fill(dtt);
+            return dtt;
+        }
         public string CreditNoteCRUD(CreditNote cy)
         {
             string msg = "";
             try
             {
-                //string StatementType = string.Empty; string svSQL = "";
-                //datatrans = new DataTransactions(_connectionString);
+                int t2cunt = 0;
+                double? grossamt = 0;
+                string Depitledger = "";
+                string Cretiledger = "";
+                double totgst = 0;
+                double? netamt = 0;
+                string ledger = string.Join("", cy.CreditNotelst.Where(x => x.DebitAmt == cy.CreditNotelst.Max(y => y.DebitAmt)).Select(x => x.AccName));
+                Cretiledger = string.Join("", cy.CreditNotelst.Where(x => x.CreditAmt == cy.CreditNotelst.Max(y => y.CreditAmt)).Select(x => x.AccName));
+
+                foreach (CreditNoteItem cp in cy.CreditNotelst)
+                {
+                    t2cunt += 1;
+                    if (cp.DBCR == "Dr")
+                    {
+                        grossamt += cp.DebitAmt;
+                    }
+
+                    else
+                    {
+                        netamt += cp.CreditAmt;
+                        //Cretiledger = cp.AccName;
+                    }
+                }
 
 
-                //int idc = datatrans.GetDataId("SELECT LAST_NUMBER FROM SEQUENCE WHERE PREFIX = 'GRN' AND IS_ACTIVE = 'Y'");
-                //string docno = string.Format("{0}{1}{2}", "GRN/", "24-25/", (idc + 1).ToString());
+                string StatementType = string.Empty;
+                string svSQL = "";
 
-                //string updateCMd = " UPDATE SEQUENCE SET LAST_NUMBER ='" + (idc + 1).ToString() + "' WHERE PREFIX ='GRN' AND IS_ACTIVE ='Y'";
-                //try
-                //{
-                //    datatrans.UpdateStatus(updateCMd);
-                //}
-                //catch (Exception ex)
-                //{
-                //    throw ex;
-                //}
+                if (cy.ID == null)
+                {
+                    datatrans = new DataTransactions(_connectionString);
+
+
+                    int idc = datatrans.GetDataId(" SELECT LAST_NUMBER FROM SEQUENCE WHERE PREFIX = 'PO' AND IS_ACTIVE = 'Y'");
+                    string VocNo = string.Format("{0}{1}{2}", "PO/", "24-25/", (idc + 1).ToString());
+
+                    string updateCMd = " UPDATE SEQUENCE SET LAST_NUMBER ='" + (idc + 1).ToString() + "' WHERE PREFIX ='PO' AND IS_ACTIVE ='Y'";
+                    try
+                    {
+                        datatrans.UpdateStatus(updateCMd);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                    cy.VocNo = VocNo;
+                }
 
                 using (SqlConnection objConn = new SqlConnection(_connectionString))
                 {
-                    string SvSql1 = "Insert into ACC_VOUCHER (VOUCH_NO,VOUCH_DATE,EXCHANGE_RATE,REF_ID,REF_DATE,REF_NO,REF_DATE,CURRENCY,AMT,AMOUNT_IN_WORD,VOUCH_MEMO,IS_ACTIVE) VALUES ('" + cy.VocNo + "' ,'" + cy.VocDate + "','" + cy.exchangeRate + "','" + cy.RefNo + "','" + cy.RefDate + "','" + cy.Currency + "','" + cy.totamt + "','" + cy.AmtWd + "','" + cy.Narr + "','" + cy.ID + "','Y') RETURNING ID INTO :GRNDEID";
-                    SqlCommand objCmdsss = new SqlCommand(SvSql1, objConn);
-                    objCmdsss.Parameters.Add(new SqlParameter("GRNDEID", SqlDbType.Int));                   //'" + docno + "',
-                    objCmdsss.Parameters["GRNDEID"].Direction = ParameterDirection.ReturnValue;
-                    objConn.Open();
-                    objCmdsss.ExecuteNonQuery();
-
-                    string stkid = objCmdsss.Parameters["GRNDEID"].Value.ToString();
-                    if (cy.ID != null)
+                    SqlCommand objCmd = new SqlCommand("AccVoucherProc", objConn);
+                    objCmd.CommandType = CommandType.StoredProcedure;
+                    if (cy.ID == null)
                     {
-                        stkid = cy.ID;
+                        StatementType = "Insert";
+                        objCmd.Parameters.Add("@id", SqlDbType.NVarChar).Value = DBNull.Value;
                     }
-                    foreach (CreditNoteItem cp in cy.CreditNotelst)
+                    else
                     {
-                        string SvSql2 = "Insert into ACC_VOUCHER_DETAIL (VOUCH_BASIC_ID,TRANS_TYPE,ACCOUNT_NAME,DEBIT_AMT,CREDIT_AMT,BALANCE) VALUES ('" + stkid + "','" + cp.DBCR + "','" + cp.AccName + "','" + cp.DebitAmt + "','" + cp.CreditAmt + "','" + cp.Balance + "')";
-                        SqlCommand objCmddts = new SqlCommand(SvSql2, objConn);
+                        StatementType = "Update";
+                        objCmd.Parameters.Add("@id", SqlDbType.NVarChar).Value = cy.ID;
+                    }
+                    objCmd.Parameters.Add("@vouchno", SqlDbType.NVarChar).Value = cy.VocNo;
+                    objCmd.Parameters.AddWithValue("@vouchdate", cy.VocDate);
+                    //objCmd.Parameters.Add("@excrate", SqlDbType.NVarChar).Value = cy.ExcRate;
+                    //objCmd.Parameters.Add("@refno", SqlDbType.NVarChar).Value = cy.RefNo;
+                    //objCmd.Parameters.AddWithValue("@refdate", cy.RefDate);
+                    //objCmd.Parameters.Add("@currency", SqlDbType.NVarChar).Value = cy.Currency;
+                    objCmd.Parameters.Add("@totdebamt", SqlDbType.NVarChar).Value = cy.totdeb;
+                    objCmd.Parameters.Add("@totcreamt", SqlDbType.NVarChar).Value = cy.totcri;
+                    objCmd.Parameters.Add("@amtinwords", SqlDbType.NVarChar).Value = cy.AmtWd;
+                    objCmd.Parameters.Add("@narration", SqlDbType.NVarChar).Value = cy.Narr;                    
+                    objCmd.Parameters.Add("@StatementType", SqlDbType.NVarChar).Value = StatementType;
+                    try
+                    {                       
                         objConn.Open();
-                        objCmddts.ExecuteNonQuery();
+                        Object Pid = objCmd.ExecuteScalar();
+                        if (cy.ID != null)
+                        {
+                            Pid = cy.ID;
+                        }
+
+
+                        if (cy.CreditNotelst != null)
+                        {
+                            if (cy.ID == null)
+                            {
+                                foreach (CreditNoteItem cp in cy.CreditNotelst)
+                                {
+
+                                    if (cp.Isvalid == "Y")
+                                    {
+                                        svSQL = "Insert into ACC_VOUCHER_DETAIL (VOUCH_BASIC_ID,TRANS_TYPE,ACCOUNT_NAME,AMT,BALANCE) VALUES ('" + Pid + "','" + cp.DBCR + "','" + cp.AccName + "','" + cp.CreditAmt + "','" + cp.Balance + "')";
+                                        SqlCommand objCmds = new SqlCommand(svSQL, objConn);
+                                        objCmds.ExecuteNonQuery();
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                svSQL = "Delete ACC_VOUCHER_DETAIL WHERE VOUCH_BASIC_ID='" + cy.ID + "'";
+                                SqlCommand objCmdd = new SqlCommand(svSQL, objConn);
+                                objCmdd.ExecuteNonQuery();
+                                foreach (CreditNoteItem cp in cy.CreditNotelst)
+                                {
+
+                                    if (cp.Isvalid == "Y")
+                                    {
+                                        svSQL = "Insert into ACC_VOUCHER_DETAIL (VOUCH_BASIC_ID,TRANS_TYPE,ACCOUNT_NAME,AMT,BALANCE) VALUES ('" + Pid + "','" + cp.DBCR + "','" + cp.AccName + "','" + cp.CreditAmt + "','" + cp.Balance + "')";
+                                        SqlCommand objCmds = new SqlCommand(svSQL, objConn);
+                                        objCmds.ExecuteNonQuery();
+                                    }
+                                }
+                            }
+
+                        }
+
+
+
 
                     }
 
-                    //svSQL = "Insert into GRN_BASIC (GRN_NO,GRN_DATE,SUP_NAME,ADDRESS,COUNTRY,STATE,CITY,REF_NO,REF_DATE,AMTINWORDS,NARRATION,TRANS_SPORTER,LR_NO,LR_DATE,PLACE_DIS,GROSS,NET,DISCOUNT,FRIGHTCHARGE,CGST,SGST,IGST,ROUNT_OFF,POBASIC_ID,IS_ACTIVE) (Select '" + docno + "','" + DateTime.Now.ToString("dd-MMM-yyyy") + "',SUP_NAME,ADDRESS,COUNTRY,STATE,CITY,REF_NO,REF_DATE,AMTINWORDS,NARRATION,TRANS_SPORTER,LR_NO,LR_DATE,PLACE_DIS,GROSS,NET,DISCOUNT,FRIGHTCHARGE,CGST,SGST,IGST,ROUNT_OFF,'" + cy.ID + "','Y' from POBASIC where POBASICID='" + cy.ID + "')";
-                    //SqlCommand objCmd = new SqlCommand(svSQL, objConn);
-                    //try
-                    //{
-                    //    objConn.Open();
-                    //    objCmd.ExecuteNonQuery();
-                    //}
-                    //catch (Exception ex)
-                    //{
-                    //    //System.Console.WriteLine("Exception: {0}", ex.ToString());
-                    //}
-                    //objConn.Close();
+                    catch (Exception ex)
+                    {
+                        System.Console.WriteLine("Exception: {0}", ex.ToString());
+                    }
+                    objConn.Close();
                 }
-
-                //string quotid = datatrans.GetDataString("Select GRN_BASIC_ID from GRN_BASIC Where POBASIC_ID=" + cy.ID + "");
-                //using (SqlConnection objConnT = new SqlConnection(_connectionString))
-
-                //{
-                //    string Sql = "Insert into GRN_DETAIL (GRN_BASIC_ID,ITEM,VARIANT,HSN,TARIFF,UOM,QTY,RATE,AMOUNT,FRIGHT,DIS_AMOUNT,CGSTP,SGSTP,IGSTP,CGST,SGST,IGST,TOTAL_AMOUNT) (Select '" + quotid + "',ITEM,VARIANT,HSN,TARIFF,UOM,QTY,RATE,AMOUNT,FRIGHT,DIS_AMOUNT,CGSTP,SGSTP,IGSTP,CGST,SGST,IGST,TOTAL_AMOUNT FROM PODETAIL WHERE POBASICID=" + cy.ID + ")";
-                //    SqlCommand objCmds = new SqlCommand(Sql, objConnT);
-                //    objConnT.Open();
-                //    objCmds.ExecuteNonQuery();
-                //    objConnT.Close();
-                //}
-
-                using (SqlConnection objConnE = new SqlConnection(_connectionString))
-                {
-                    string Sql = "UPDATE POBASIC SET STATUS='GRN Generated' where POBASICID='" + cy.ID + "'";
-                    SqlCommand objCmds = new SqlCommand(Sql, objConnE);
-                    objConnE.Open();
-                    objCmds.ExecuteNonQuery();
-                    objConnE.Close();
-                }
-
             }
             catch (Exception ex)
             {
@@ -112,9 +176,29 @@ namespace RetailSales.Services.Accounts
             }
 
             return msg;
+        }       
+        public DataTable GetEditCreditNote(string id)
+        {
+            string SvSql = string.Empty;
+            SvSql = "SELECT PONO,CONVERT(varchar, POBASIC.PODATE, 106) AS PODATE,SUP_NAME,POBASIC.ADDRESS,POBASIC.COUNTRY,POBASIC.STATE,POBASIC.CITY,REF_NO,CONVERT(varchar, POBASIC.REF_DATE, 106) AS REF_DATE,AMTINWORDS,NARRATION,TRANS_SPORTER,LR_NO,CONVERT(varchar, POBASIC.LR_DATE, 106) AS LR_DATE,PLACE_DIS,GROSS,NET,CGST,SGST,IGST,ROUNT_OFF,DISCOUNT,FRIGHTCHARGE FROM POBASIC WHERE POBASIC.POBASICID='" + id + "'";
+            DataTable dtt = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(SvSql, _connectionString);
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            adapter.Fill(dtt);
+            return dtt;
         }
 
-
+        
+        public DataTable GetCreditNoteDetailes(string id)
+        {
+            string SvSql = string.Empty;
+            SvSql = "SELECT POBASICID,PODETAILID,PRODUCT.PRODUCT_NAME,PRO_DETAIL.PRODUCT_VARIANT,HSN,PODETAIL.TARIFF,PODETAIL.UOM,QTY,PODETAIL.RATE,AMOUNT,FRIGHT,DIS_AMOUNT,CGSTP,SGSTP,IGSTP,CGST,SGST,IGST,TOTAL_AMOUNT FROM PODETAIL LEFT OUTER JOIN PRODUCT ON PRODUCT.ID=PODETAIL.ITEM LEFT OUTER JOIN PRO_DETAIL ON PRO_DETAIL.ID=PODETAIL.VARIANT WHERE PODETAIL.POBASICID='" + id + "'";
+            DataTable dtt = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(SvSql, _connectionString);
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            adapter.Fill(dtt);
+            return dtt;
+        }
     }
 
 }
