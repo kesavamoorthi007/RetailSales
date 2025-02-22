dacdc964201006a13d7404e0949abb053b366e15
devteam845 <devteam@icand.in>
2025-02-22 15:46:31 +0530

    Complete

--- RetailSales/Interface/Master/IRateService.cs
+++ RetailSales/Interface/Master/IRateService.cs
@@ -5,22 +5,25 @@ namespace RetailSales.Interface.Master
 {
     public interface IRateService
     {
-        public DataTable GetItem();
+        //public DataTable GetItem();
 
-        public DataTable GetItemDetails(string ItemId);
+        //public DataTable GetItemDetails(string ItemId);
 
         public DataTable GetAllRate(string strStatus);
 
+        DataTable GetproductDetail(string id);
+
+        string RateCRUD(Rate cy);
+
         public DataTable GetEditRate(string id);
-       
+
+        public DataTable GetEditRateDetail(string id);
+
         public string RemoveChange(string tag, string id);
 
         public string StatusChange(string tag, string id);
 
-        string RateCRUD(Rate cy);
-
-        public DataTable GetEditRateDetail(string id);
-
-        public DataTable GetVariant(string id);
+        //public DataTable GetVariant(string id);
+        
     }
 }
