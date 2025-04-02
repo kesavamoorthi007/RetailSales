d8c36586afce72b5a2c0d410d8c9389d79970079
devteam845 <devteam@icand.in>
2025-04-02 14:08:54 +0530

    Correction

--- RetailSales/Services/Purchase/PurchaseorderService.cs
+++ RetailSales/Services/Purchase/PurchaseorderService.cs
@@ -97,7 +97,7 @@ namespace RetailSales.Services.Purchase
         public DataTable GetVariant(string id)
         {
             string SvSql = string.Empty;
-            SvSql = "SELECT PRO_DETAIL.ID,PRODUCT_VARIANT FROM PRO_DETAIL WHERE PRO_DETAIL.PRODUCTS_NAME='" + id + "' AND PRO_DETAIL.IS_ACTIVE = 'Y'";
+            SvSql = "SELECT PRO_DETAIL.ID,PRODUCT_VARIANT FROM PRO_DETAIL WHERE PRO_DETAIL.PRO_NAME_BASICID='" + id + "' AND PRO_DETAIL.IS_ACTIVE = 'Y'";
             //SvSql = "SELECT ID,CONCAT(PRODUCT_VARIANT, ' - ', HSCODE) AS Variant_HSN FROM PRO_DETAIL JOIN HSNMAST ON HSN_CODE = HSNMASTID WHERE PRO_DETAIL.PRODUCT_CATEGORY='" + id + "'";
             DataTable dtt = new DataTable();
             SqlDataAdapter adapter = new SqlDataAdapter(SvSql, _connectionString);
