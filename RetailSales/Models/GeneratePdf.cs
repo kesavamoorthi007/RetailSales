9c6c833e260638b3a0b572ec07c0799b03bc1665
devteam845 <devteam@icand.in>
2025-04-18 19:07:34 +0530

    Complete

---
+++ RetailSales/Models/GeneratePdf.cs
@@ -0,0 +1,18 @@
+﻿namespace RetailSales.Models
+{
+    public class GeneratePdf
+    {
+        public string ID { get; set; }
+        public string PoNo { get; set; }
+        public string PoDate { get; set; }
+        public string CompName { get; set; }
+        public string CompAdd { get; set; }
+        public string SuppName { get; set; }
+        public string SuppAdd { get; set; }
+        public string GSTNo { get; set; }
+        public string Category { get; set; }
+        public string Product { get; set; }
+        public string Variant { get; set; }
+        public string Qty { get; set; }
+    }
+}
